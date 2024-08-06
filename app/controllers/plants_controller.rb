a38e5ff40ee9e145d75c1be911c85a726572afdc
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-06 11:30:26 -0400

    Add update and destroy routes & actions

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,4 @@
 class PlantsController < ApplicationController
-
   # GET /plants
   def index
     plants = Plant.all
@@ -8,7 +7,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant_by_id
     render json: plant
   end
 
@@ -18,9 +17,24 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant_by_id
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = find_plant_by_id
+    plant.destroy
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant_by_id
+    Plant.find_by(id: params[:id])
   end
 end
